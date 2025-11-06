acf29a9223a69c59312a7e2a4b3a725a31bfb8ec
Цено Стаменов <tsenostamenov@gmail.com>
2025-11-07 00:09:40 +0200

    Enhance group listing with readable Type column

--- Get-All-Groups-Detailed.ps1
+++ Get-All-Groups-Detailed.ps1
@@ -1,8 +1,20 @@
 # Shared by: Tseno Stamenov
-# Lists all groups with detailed properties like MailEnabled, SecurityEnabled, and GroupTypes for easier identification.
+# Lists all groups with flattened GroupTypes and a readable Type column
 
 Connect-MgGraph -Scopes "Group.Read.All"
 
 Get-MgGroup -All |
-Select DisplayName, MailEnabled, SecurityEnabled, GroupTypes |
+Select-Object `
+  DisplayName,
+  MailEnabled,
+  SecurityEnabled,
+  @{Name='GroupTypes'; Expression={ ($_.GroupTypes -join ';') }},     # flatten array
+  @{Name='Type'; Expression={
+      if     ($_.GroupTypes -contains 'Unified')              {'M365 Group'}
+      elseif ($_.GroupTypes -contains 'DynamicMembership')    {'Dynamic Group'}
+      elseif ($_.MailEnabled -and  $_.SecurityEnabled)        {'Mail-enabled Security'}
+      elseif ($_.MailEnabled -and -not $_.SecurityEnabled)    {'Distribution'}
+      elseif (-not $_.MailEnabled -and $_.SecurityEnabled)    {'Security'}
+      else                                                     {'Other'}
+  }} |
 Export-Csv "$env:USERPROFILE\Documents\Groups_Detailed.csv" -NoTypeInformation -Encoding UTF8
